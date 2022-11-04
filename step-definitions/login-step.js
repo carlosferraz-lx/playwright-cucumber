d3680a1298e195b30c1295d08341795795f8beef
CarlosFerrazSF <carlos@studentfinance.com>
2022-11-04 17:50:57 +0000

    Update login-step.js

--- step-definitions/login-step.js
+++ step-definitions/login-step.js
@@ -1,27 +1,27 @@
-const { Given, When, Then, defineStep } = require('@cucumber/cucumber')
-const { LoginPage } = require('../page-objects/login-page')
+const { Given, When, Then, defineStep } = require('@cucumber/cucumber');
+const { LoginPage } = require('../page-objects/login-page');
 
-const loginPage = new LoginPage()
+const loginPage = new LoginPage();
 
-defineStep('I visit a login page', async function () {
-  await loginPage.navigateToLoginScreen()
-})
+Given('I visit a login page', async function () {
+  await loginPage.navigateToLoginScreen();
+});
 
-defineStep('I fill the login form with valid credentials', async function () {
-  await loginPage.submitLoginForm()
-})
+When('I fill the login form with valid credentials', async function () {
+  await loginPage.submitLoginForm();
+});
 
-defineStep('I should see the home page', async function () {
-  await loginPage.assertUserIsLoggedIn()
-})
+Then('I should see the home page', async function () {
+  await loginPage.assertUserIsLoggedIn();
+});
 
-defineStep('I wait for 3 seconds', async function () {
-  await loginPage.pause()
-})
+Then('I wait for 3 seconds', async function () {
+  await loginPage.pause();
+});
 
-defineStep(
+When(
   /^I fill the login form with "([^"]*)" and "([^"]*)"$/,
   async function (username, password) {
-    await loginPage.submitLoginWithParameters(username, password)
+    await loginPage.submitLoginWithParameters(username, password);
   }
-)
+);
