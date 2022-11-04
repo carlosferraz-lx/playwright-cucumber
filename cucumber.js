8848c208ec3088cb5a4df151b78d2cbcbc7e8cce
CarlosFerrazSF <carlos@studentfinance.com>
2022-11-04 17:42:28 +0000

    removing files

--- cucumber.js
+++
@@ -1,9 +0,0 @@
-const common = `
-    --require setup/assertions.js
-    --require setup/hooks.js
-    --require step-definition/**/*.step.js
-    `
-
-module.exports = {
-  default: `${common} features/**/*.feature`,
-}
